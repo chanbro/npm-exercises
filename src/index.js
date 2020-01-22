e80ee23e3cded255ca4a08e2992a653a05aeeb9e
Chaney <52712847+chanbro@users.noreply.github.com>
2020-01-22 10:39:42 -0600

    added module exercise

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-const sayHello = () => console.log('hello');
-sayHello();
+'use strict';
 
 const $ = require('jquery');
 
+const greeting = require('./say-hello.js');
+
+console.log(greeting.sayHello());
+// const sayHello = () => console.log('hello');
+// sayHello();
+
 $('body').css('background-color', 'black');
+
+$('body').html('<h1>Hello</h1>');
+
+$('body').css('color', 'white');
+
+
+
+
---
+++ src/say-hello.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const sayHello = () => 'hello hi';
+
+module.exports = {sayHello};
