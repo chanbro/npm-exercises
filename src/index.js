6098429db69e843c450cecddcf794a98e812adf0
Chaney <52712847+chanbro@users.noreply.github.com>
2020-01-22 14:42:34 -0600

    updated import/export functionality

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@
 
 const $ = require('jquery');
 
-const greeting = require('./say-hello.js');
+// const {greeting, power, banana} = require('./say-hello.js');
+
+import greeting from './say-hello.js'
+import power from './say-hello.js'
+import banana from './say-hello.js'
+
 
 console.log(greeting.sayHello());
+console.log(power.power(2, 4));
+console.log(banana);
+
 // const sayHello = () => console.log('hello');
 // sayHello();
 
